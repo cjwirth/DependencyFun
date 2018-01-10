848015073a8defd00eaaa5a434e7e3be34341cad
Caesar Wirth <cjwirth@gmail.com>
2018-01-09 22:11:42 -0500

    Initial commit

---
+++ InternalDependency/InternalDependency/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  InternalDependency
+//
+//  Created by Caesar Wirth on 1/9/18.
+//  Copyright © 2018 Caesar Wirth. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
